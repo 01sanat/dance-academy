165220bf15df8c3ec4470b7af21812e31f5c5949
Sanat Sharma <sharmasanat01@gmail.com>
2021-06-19 07:30:23 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+var length=document.querySelectorAll("button").length
+
+for(var i=0;i<length;i++)
+{
+document.querySelectorAll("button")[i].addEventListener("click",function(){
+var button=this.innerHTML;
+makesound(button);
+animation(button);
+    
+});
+}
+document.addEventListener("keypress",function(event){
+  makesound(event.key);
+  animation(event.key);
+});
+
+
+function makesound(key)
+{    
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();   
+            
+            break;
+       case "a":
+        var audio=new Audio("sounds/tom-2.mp3");
+        audio.play();
+        break;
+
+        case "s":
+            var audio=new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio= new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+         case "j":
+            var audio= new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+             
+         case "k":
+            var audio= new Audio();
+            audio.src="sounds/snare.mp3";
+            audio.play();
+            break;
+
+         case "l":
+            var audio= new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        default:
+            break;
+    }
+}
+
+function animation(currentkey)
+{
+   var activebutton= document.querySelector( "."+currentkey );
+   activebutton.classList.add("pressed");
+   setTimeout(function(){
+       activebutton.classList.remove("pressed");
+   },100);
+
+}
+$("h1").on("mouseover",function(){
+    $("h1").css("color","black");
+});
+
+
